cc63b058e448e4ea95ace89889f9a1da140d087d
I-am-Som <somanathmaharanaofficial@gmail.com>
2025-02-18 15:46:24 +0530

    ...

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="h-screen w-screen bg-slate-900 flex items-center justify-center px-4">
+      <div className="h-[60%] w-[90%] sm:w-[70%] md:w-[50%] lg:w-[30%] rounded-lg bg-slate-800 flex items-center justify-center p-4">
+        <p className="text-amber-200 font-semibold font-sans text-center text-lg">
+          Hi, this is me uploading to Vercel
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
